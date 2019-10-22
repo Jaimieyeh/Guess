ced92f1f49bd3e7476f56f2af6eeb9c7b5f576ea
Jaimieyeh <43363033+Jaimieyeh@users.noreply.github.com>
2019-10-22 20:16:50 +0800

    Update MainActivity.java
    var

--- app/src/main/java/com/jaimie/guess/MainActivity.java
+++ app/src/main/java/com/jaimie/guess/MainActivity.java
@@ -1,18 +1,23 @@
 package com.jaimie.guess;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int secret = new Random().nextInt(10) + 1;
+    private EditText enter;
+    private TextView message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
+        enter = findViewById(R.id.guess);
+        message = findViewById(R.id.result);
+       FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void turn (View view){
+        int guess = Integer.parseInt(enter.getText().toString());
+        if (guess > secret){
+            message.setText("Smaller");
+        }else if (guess < secret){
+            message.setText("Bigger");
+        }else if (guess == secret){
+            message.setText("Bingo!");
+        }
     }
 
     @Override
