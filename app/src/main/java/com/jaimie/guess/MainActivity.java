6a479a1f90725288eff37b4e1387a2b952b24e2d
miia <miia@com.tw>
2019-10-29 16:30:57 +0800

    var

--- app/src/main/java/com/jaimie/guess/MainActivity.java
+++ app/src/main/java/com/jaimie/guess/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jaimie.guess;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
@@ -15,8 +18,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int secret = new Random().nextInt(10) + 1;
+    int counter = 0;
     private EditText enter;
     private TextView message;
+    private TextView edcounter;
 
 
     @Override
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         enter = findViewById(R.id.guess);
         message = findViewById(R.id.result);
+        edcounter = findViewById(R.id.counter);
+        edcounter.setText(counter + "");
        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +47,27 @@ public class MainActivity extends AppCompatActivity {
     public void turn (View view){
         int guess = Integer.parseInt(enter.getText().toString());
         if (guess > secret){
-            message.setText("Smaller");
+           new AlertDialog.Builder(MainActivity.this)
+                   .setTitle("Hahaha")
+                   .setMessage("Smaller")
+                   .setPositiveButton("OK", null)
+                   .show();
         }else if (guess < secret){
             message.setText("Bigger");
         }else if (guess == secret){
-            message.setText("Bingo!");
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Hahaha")
+                    .setMessage("Bigger")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            secret = new Random().nextInt(10) + 1;
+                            counter ++;
+                            edcounter.setText( counter + "");
+
+                        }
+                    })
+                    .show();
         }
     }
 
